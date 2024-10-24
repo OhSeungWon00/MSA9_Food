6b10a5263c99290a8e430d333d70b80c741f1feb
rlawnsrud310 <wnsrud310@gmail.com>
2024-10-24 10:47:19 +0900

    Update newBrandService.java

--- food/src/main/java/food/Service/newBrandService.java
+++ food/src/main/java/food/Service/newBrandService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package food.Service;
 
 import food.DTO.newBrand;
@@ -7,17 +6,12 @@ public interface newBrandService {
 	
 	// 매장등록
 	public int newbrand(newBrand newbrand);
-
-}
-=======
-package food.Service;
-
-import food.DTO.newBrand;
-
-public interface newBrandService {
 	
-	// 매장등록
-	public int newbrand(newBrand newbrand);
+	// 매장검색
+	public newBrand search(newBrand newbrand);
+	
+	// 매장수정
+	public int brandUpdate(newBrand newBrand);
+	
 
 }
->>>>>>> refs/heads/main
