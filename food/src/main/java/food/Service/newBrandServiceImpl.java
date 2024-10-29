0c0746141945ea6279d621d93117fe7a3b0baef7
rlawnsrud310 <wnsrud310@gmail.com>
2024-10-29 15:24:46 +0900

    Update newBrandServiceImpl.java

--- food/src/main/java/food/Service/newBrandServiceImpl.java
+++ food/src/main/java/food/Service/newBrandServiceImpl.java
@@ -1,122 +1,59 @@
 package food.Service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.alohaclass.jdbc.dto.Page;
 import com.alohaclass.jdbc.dto.PageInfo;
 
-import food.DAO.newBrandDAO;
-import food.DTO.User;
-import food.DTO.newBrand;
+import food.DAO.ReviewDAO;
+import food.DTO.Review;
 
-public class newBrandServiceImpl implements newBrandService{
+public class ReviewServiceImpl implements ReviewService{
 	
-	newBrandDAO newbrandDAO = new newBrandDAO();
-	
+	ReviewDAO reviewDAO = new ReviewDAO();
+
 	@Override
-	public int newbrand(newBrand newbrand) {
+	public int newreview(Review review) {
 		int result = 0;
 		
 		try {
-			result = newbrandDAO.insert(newbrand, "name", "location", "phone", "content");
+			result = reviewDAO.insert(review, "rating","content","reg_date","userId","name");
 		} catch (Exception e) {
-			System.out.println("생성오류");
+			System.out.println("작성오류");
 			e.printStackTrace();
 		}
 		if(result == 0) {
 			return 0;
-		}		
-		
-		return result;
-	}
-
-	@Override
-	public newBrand search(newBrand newbrand) {
-		String name = newbrand.getName();
-		newBrand selectedBrandName = null;
-		System.out.println(" name : " + name);
-		
-			try {
-				Map<Object, Object> where = new HashMap<Object, Object>();
-				where.put("name", name);
-				selectedBrandName = newbrandDAO.selectBy(where); // pk 출력
-				System.out.println("-- : " + selectedBrandName);
-			} catch (Exception e) {
-				System.out.println("호출오류");
-				e.printStackTrace();
-			}
-			return selectedBrandName;
-	}
-
-	@Override
-	public int brandUpdate(newBrand newBrand) {
-		int result = 0;
-		
-		try {
-			result = newbrandDAO.update(newBrand);
-			System.out.println("Brand -- " + newBrand);
-			System.out.println("update 성공");
-			
-		} catch (Exception e) {
-			System.out.println("업뎃오류");
-			e.printStackTrace();
 		}
-		
-		if(result == 0) {
-			return 0;
-		}		
-		
+		// TODO Auto-generated method stub
 		return result;
-		
 	}
 
 	@Override
-	public List<newBrand> list() {
-		List<newBrand> list = null;
+	public List<Review> list() {
+		List<Review> list = null;
+		
 		try {
-			list = newbrandDAO.list();
+			list = reviewDAO.list();
 		} catch (Exception e) {
 			System.out.println("list 생성오류");
 			e.printStackTrace();
 		}
-		
 		return list;
 	}
 
 	@Override
-	public int delete(newBrand newbrand) {
-		int result = 0;
-		try {
-			result = newbrandDAO.delete(newbrand.getName() );
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-				
-		return result;
-	}
-
-	@Override
-	public PageInfo<newBrand> page(int page) {
+	public PageInfo<Review> page(int page) {
 		Page pageObj = new Page();
 		pageObj.setPage(page);
 		pageObj.setSize(7);
-        System.out.println("------------------------------");
-        System.out.println(pageObj);
-        PageInfo<newBrand> pageInfo = null;
-        try {
-			pageInfo = newbrandDAO.page(pageObj);
+		PageInfo<Review> pageInfo = null;
+		try {
+			pageInfo = reviewDAO.page(pageObj);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-        return pageInfo;
+		return pageInfo;
 	}
-
-
-
-
-
-
 
 }
